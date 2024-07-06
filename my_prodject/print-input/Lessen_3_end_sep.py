4bb984ea37f72c76a8cacc9ad3752a2216106e89
Станислав <morevstas02@mail.ru>
2024-07-06 15:54:18 +0300

    Примечание

--- my_prodject/print-input/Lessen_3_end_sep.py
+++ my_prodject/print-input/Lessen_3_end_sep.py
@@ -1,2 +1,19 @@
-# sep
-print('aa', 'bb', 'cc', sep='*')
+
+# # sep c переменно
+# minus = '-'
+# print('aa', 'bb', 'cc', sep=minus)
+# параметр end
+# print("A great man doesn't seek to lead.")
+# print("He's called to it. And he answers.")
+#
+#
+# print("A great man doesn't seek to lead.", end='\n')
+# print("He's called to it. And he answers.", end='\n')
+
+# minus = '-'
+# print('a', 'b', 'c', end=minus)
+# print('second line')
+#
+# print('a', '\n', 'b', '\n', 'c', sep='*', end='#')
+
+# print('a', 'b', 'c', sep='*', end='finich')
