7f7e6534bb6f32deb919cc11fb3bb98015bc7a44
Станислав <morevstas02@mail.ru>
2024-07-15 21:22:37 +0300

    Логические цепочки

---
+++ my_prodject/conditional_operators/Lessen_5.py
@@ -0,0 +1,27 @@
+# первый пример
+# answer = input('Какой язык программирования мы изучаем\n')
+# if answer == 'Python':
+#     print("Да мы изучаем Python")
+# else:
+#     pass
+
+
+# answer = input('Какой язык программирования мы изучаем\n')
+# if answer == 'Python':
+#     print("Да мы изучаем Python")
+# else:
+#     print('Не совсем так!')
+
+# num1 = int(input())
+# num2 = int(input())
+#
+# if num2 < num2:
+#     print(f'{num1} больше чем {num2}')
+# if num1 > num2:
+#     print(f'{num1} меньше чем {num2}')
+#
+# if num1 == num2:
+#     print(f'{num1} равно {num2}')
+
+# if num1 != num2:
+#     print(f'{num1} не равно {num2}')
