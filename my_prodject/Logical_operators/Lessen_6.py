344624097733b1d3358185a5f4bd41b53c8730c1
Станислав <morevstas02@mail.ru>
2024-07-20 19:07:11 +0300

    Оператор or

--- my_prodject/Logical_operators/Lessen_6.py
+++ my_prodject/Logical_operators/Lessen_6.py
@@ -1,9 +1,8 @@
-# Логические операторы
 age = int(input('Сколько вам лет: '))
 grade = int(input('В каком классе вы учитесь?: '))
+city = input('В каком городе вы живете?')
 
-
-if age >= 12 and grade >= 7:
+if age >= 12 and grade >= 7 and city == 'Донецк':
     print('Доступ разрешен')
 else:
     print('Доступ запрещён')
