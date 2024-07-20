596ccc2c50098a1fdaec3b85befb42152a99e9c9
Станислав <morevstas02@mail.ru>
2024-07-21 00:45:30 +0300

    Задачи логические операторы

--- my_prodject/Logical_operators/Lessen_6.py
+++ my_prodject/Logical_operators/Lessen_6.py
@@ -1,7 +1,18 @@
-city = input('В каком городе вы живете?:\t')
-if city == 'Москва' or city == 'Донецк':
-    print('Доступ разрешён')
+# age = int(input('Cколько вам лет: '))
+# if not (age < 12):
+#     print('Доступ разрешен')
+#
+# else:
+#     print('Допступ запрещен')
+
+
+a = int(input())
+
+if a >= 2 and a <= 17:
+    b = 3
+    p = a * a + b * b
 else:
-    print('Доступ запрещён')
+    b = 5
 
-
+p = (a + b) * (a + b)
+print(p)
