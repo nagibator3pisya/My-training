8762c9ed5165dd9c896970eacb26f8f332a7920a
Станислав <morevstas02@mail.ru>
2024-07-20 17:35:25 +0300

    Логические операторы

---
+++ my_prodject/conditional_operators/Lessen_5_Tasks.py
@@ -0,0 +1,54 @@
+# Пароль
+
+# password = str(input())
+# password2 = str(input())
+#
+# if password == password2:
+#     print('Пароль принят')
+# else:
+#     print("Пароль не принят")
+
+# Проверка на четность числа или не четность
+
+# num = int(input())
+#
+# if num % 2 == 0:
+#     print('Четное')
+# else:
+#     print('Нечетное')
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# otvet = 2 * b == a + c
+# if otvet:
+#     print('YES')
+# else:
+#     print('NO')
+# num1 = int(input)
+# num2 = int(input)
+# num3 = int(input)
+# num4 = int(input)
+#
+# print(min(num1, num2, num3, num4))
+
+# age = int(input())
+# if age <= 13:
+#     print('детство')
+# if 14 >= age == 24:
+#     print('молодость')
+#
+# if 25 >= age == 59:
+#     print('зрелость')
+#
+# if age > 60:
+#     print('Старость')
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+summ = sum(num1,num2,num3)
+
+
+
