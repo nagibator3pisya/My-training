9f0baa9a9636a688cbbd836744113be93a4c39a6
Станислав <morevstas02@mail.ru>
2024-07-22 21:04:33 +0300

    Решение задач

--- my_prodject/Logical_operators/Lessen_7.py
+++ my_prodject/Logical_operators/Lessen_7.py
@@ -1,16 +1,32 @@
-# Вложенные условия
+# Каскадное ветвление
 
-# x = int(input())
-# y = int(input())
+# grade = int(input('Введите вашу отметку: '))
 #
-# if x > 0:
-#     if y > 0:
-#         print('Первая четверть')
-#     else:
-#         print('Четвертая четверть')
+# if grade >= 90:
+#     print(5)
+# elif grade >= 80:
+#     print(4)
+# elif grade >= 70:
+#     print(3)
+# elif grade >= 60:
+#     print(2)
+
+#Гонка спидстеров
+# Зума равна   n, а скорость Флэша равна  k
+# n = int(input())
+# k = int(input())
 #
-# else:
-#     if y > 0:
-#         print('Вторая четверть')
-#     else:
-#         print('Третья четверть')
+# if n > k :
+#     print('NO')
+# elif k > n:
+#     print('YES')
+# elif k == n:
+#     print("Don't know")
+
+# Вид треугольника
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+
