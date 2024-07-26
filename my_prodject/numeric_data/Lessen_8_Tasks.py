c1bb0bb7e563feedeb7010bf2b9638ab34f070a0
Станислав <morevstas02@mail.ru>
2024-07-26 21:51:26 +0300

    Решение задач

---
+++ my_prodject/numeric_data/Lessen_8_Tasks.py
@@ -0,0 +1,35 @@
+# a = float(input())
+# b = float(input())
+#
+# s = 1 / 2 * a * b
+#
+# print(s)
+
+
+# Две старушки
+
+# s = float(input())
+# v1 = float(input())
+# v2 = float(input())
+#
+# form = v1 + v2
+# form2 = s / form
+#
+# print(form2)
+
+# Обратное число
+
+# num = float(input())
+#
+# if num / 2 == 0:
+#     print('Обратного числа не существует')
+# else:
+#     num2 = 1 / num
+#     print(num2)
+
+# градус по Фаренгейту
+
+# Tf = float(input())
+# Tc = 5 / 9 * (Tf - 32 )
+#
+# print(Tc)
