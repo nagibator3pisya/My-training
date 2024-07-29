46d995db6670369d7e4df6214926493c2a781d76
Станислав <morevstas02@mail.ru>
2024-07-29 21:20:36 +0300

    Cроковый тип данных

--- my_prodject/numeric_data/Lessen_8_Tasks.py
+++ my_prodject/numeric_data/Lessen_8_Tasks.py
@@ -1,21 +1,33 @@
-# вычисление лет собаки
-# age_dog = int(input())
-# if  age_dog <= 2:
-#     n = age_dog * 10.5
-#     print(f'Возрост собаки не меньше двух лет {n}')
-# elif age_dog >= 2:
-#     n2 = (age_dog - 2) * 4 + 21
-#     print(n2)
-
-# положительное число
+# d1, d2, d3, d4, d5 = int(input()), int(input()), int(input()), int(input()), int(input())
+# num1 = max(d1,d2,d3,d4,d5)
+# num2 = min(d1,d2,d3,d4,d5)
 #
-# num = float(input())
-# out = int(num * 10)
-# out2 = out % 10
-# print(out2)
+# print(f'Наибольшее число = {num1}\nНаименьшее число = {num2}')
 
-#  положительное действительное число. Выведите его дробную часть.
-floats = float(input())
-integers = ( floats - int(floats) ) % 1
+# упорядочить числа
 
-print(integers)
+# num1,num2,num3 = int(input()),int(input()),int(input())
+# maxc = max(num1,num2,num3)
+# mins = min(num1,num2,num3)
+# sort1 = num1 + num2 + num3 - (mins - maxc)
+# print(mins,sort1,maxc, sep='\n')
+
+
+#  Абсолютная сумма
+#
+# a1 = float(input())
+# a2 = float(input())
+# a3 = float(input())
+# a4 = float(input())
+# a5 = float(input())
+# print(abs(a1) + abs(a2) + abs(a3) + abs(a4) +abs(a5))
+
+# манхэттенское расстояние
+
+p1 = float(input())
+p2 = float(input())
+q1 = float(input())
+q2 = float(input())
+
+sum = abs(p1 - q1) + abs(p2 - q2)
+print(round(sum))
