e9a4f5c17b7efaa45b4a43ca2c46dd6fe87a86e2
Станислав <morevstas02@mail.ru>
2024-07-30 19:34:03 +0300

    Не большие задачки

---
+++ my_prodject/string_data_type/Lessen_9.py
@@ -0,0 +1,25 @@
+# длинна строки
+
+# string = 'привет'
+# num = len(string)
+# print(num)
+
+# Приобразование чисел в строку
+
+# num = 123
+# sum = str(num)
+# print(sum)
+# print(type(sum))
+
+# Сложение строк
+# str1 = 'ab'
+# str2 = 'cd'
+#
+# print(str1 + str2)
+
+# Умножение строк на число
+#
+# str = 'привет' * 5
+# print(str)
+mystr = '123' * 3 + '456' * 2 + '789' * 1
+print(mystr)
