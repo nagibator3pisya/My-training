5b03766b6eba4b8c87d03415f56aaab5e187de22
Станислав <morevstas02@mail.ru>
2024-08-13 21:49:27 +0300

    Не большие задачи

--- my_prodject/Cycle_For_While/Lessen_12.py
+++ my_prodject/Cycle_For_While/Lessen_12.py
@@ -1,22 +1,16 @@
+# num1 = 4
+# num2 = 6
+# num1 += num2
+# num1 *= num1
+# print(num1)
 
-# находит наибольшее число
-# largest = 0
-# for _ in range(3):
-#     num = int(input())
-#     if num > largest:
-#         largest = num
-#
-# print('Наибольшее число равно', largest)
+# total = 0
+# for i in range(1, 6):
+#     total += i
+# print(total)
+# 15
 
-
-#
-# largest = int(input())  # принимаем первое число за максимальное
-# for _ in range(3):
-#     num = int(input())
-#     if num > largest:
-#         largest = num
-#
-# print('Наибольшее число равно', largest)
-
-
-#
+total = 0
+for i in range(1, 6):
+    total += i
+    print(total, end='')
