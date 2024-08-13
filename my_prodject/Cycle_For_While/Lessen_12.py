585854e8b49b7dbd41bd50654442624fdd863af5
Станислав <morevstas02@mail.ru>
2024-08-13 21:43:56 +0300

    Не большие задачи

---
+++ my_prodject/Cycle_For_While/Lessen_12.py
@@ -0,0 +1,22 @@
+
+# находит наибольшее число
+# largest = 0
+# for _ in range(3):
+#     num = int(input())
+#     if num > largest:
+#         largest = num
+#
+# print('Наибольшее число равно', largest)
+
+
+#
+# largest = int(input())  # принимаем первое число за максимальное
+# for _ in range(3):
+#     num = int(input())
+#     if num > largest:
+#         largest = num
+#
+# print('Наибольшее число равно', largest)
+
+
+#
