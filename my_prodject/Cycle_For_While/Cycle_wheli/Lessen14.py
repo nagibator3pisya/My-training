855488c9ae0bfba5177f32505215b6f3f6e45093
Станислав <morevstas02@mail.ru>
2024-09-02 20:55:11 +0300

    Связка while/else

--- my_prodject/Cycle_For_While/Cycle_wheli/Lessen14.py
+++ my_prodject/Cycle_For_While/Cycle_wheli/Lessen14.py
@@ -1,39 +1,27 @@
 
-# вывод строки и так же её длинну в бесконечном цикле
-# while True:
-#     a = input()
-#     if a == 'exit':
-#         break
-#     print(a, len(a))
-
-# оператор continue
-# проверка четного числа
-# for i in range(1,11):
-#     if i % 2 == 0:
-#         continue
-#     print(i)
-
-
-# вывод числе до 10 но без 5 цикл while
-
-# i = 0
-#
-# while i <= 10:
-#     if i == 5:
-#         i += 1
-#         continue
-#     print(i)
-#     i += 1
-
-
-
-
-# while True:
-#     a = input()
-#     if a == 'exit':
-#         break
-#     if len(a) == 5:
-#         continue
-#     print(a, len(a))
-
 # Связка for/else
+
+# for i in range(5):
+#      print(i)
+# else:
+#     print("Числа закончились")
+
+# # c break и else
+#
+# for i in range(5):
+#     if i == 3:
+#         break
+#     else:
+#         print(i)
+# else:
+#     print("Числа закончились")
+
+# c continue  и else
+
+for i in range(5):
+    if i == 3:
+        continue
+    else:
+        print(i)
+else:
+    print("Числа закончились")
