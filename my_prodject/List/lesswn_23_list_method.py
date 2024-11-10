e3b688c01d48218e59bdb6e17b1cd7cb233603d7
Станислав <morevstas02@mail.ru>
2024-11-10 16:34:15 +0300

    24 less split or join

--- my_prodject/List/lesswn_23_list_method.py
+++ my_prodject/List/lesswn_23_list_method.py
@@ -76,36 +76,97 @@
 # print(*r,sep ='\n')
 
 # Google search - 1
-
-numbers = int(input())
-string = []
-for i in range(numbers):
-    sting_gogle = input()
-    string.append(sting_gogle)
-
-
-
-chear = input()
-
-for j in string:
-    if chear.lower() in j.lower():
-        print(j)
-    else:
-        continue
-
-
-
+# numbers = int(input())
+# string = []
+# for i in range(numbers):
+#     sting_gogle = input()
+#     string.append(sting_gogle)
 #
-# poists = input()
-# poist = []
+# print(string)
+#
+# chear = input()
 #
 # for j in string:
-#     if poists in j:
-#         poist.append(j)
+#     if chear.lower() in j.lower():
+#         print(j)
+#     else:
+#         continue
+
+
+
+
+# Google search - 2
+
+'''
+    идет кол во строк число
+    далее количество строк по числу вывод
+
+    k — количество поисковых запросов
+    и вывод k строк — поисковые запросы
+'''
+
+# numbers = int(input())
+# string = []
+# for i in range(numbers):
+#     sting_gogle = input()
+#     string.append(sting_gogle)
 #
-# print(poist)
+#
+# chear = input()
+# # k_int = int(input())
+# for j in string:
+#     if chear.lower() in j.lower():
+#         print(j)
+#     else:
+#         continue
+
+
+# nubers = int(input()) # Вводим количество строк
+#
+# a_sting = [] # Список для строк
+# b_chear_reqvest = [] # Список для поисковых запросов
+# c = [] # Список для строк, содержащих все запросы
+#
+# for i in range(nubers):
+#   string = input() # Вводим строку
+#   a_sting.append(string) # Добавляем строку в список
+#
+# k_nub = int(input()) # Вводим количество поисковых запросов
+#
+# for _ in range(k_nub): # Вводим поисковые запросы
+#   chear_reqvest = input()
+#   b_chear_reqvest.append(chear_reqvest.lower()) # Добавляем запрос в нижнем регистре
+#
+# # Проверка на наличие всех запросов в каждой строке
+# for u in range(len(a_sting)):
+#   count = 0
+#   for j in b_chear_reqvest:
+#     if j in a_sting[u].lower(): # Проверяем, содержится ли запрос в строке
+#       count += 1
+#   if count == len(b_chear_reqvest): # Если все запросы найдены
+#     c.append(a_sting[u]) # Добавляем строку в список c
+#
+# # Выводим список c
+# print(*c, sep='\n')
 
 
 
+# Negatives, Zeros and Positives
 
+numbers = int(input())
+negative_results = [] # отрицательные
+zero = [] # нули
+positive_numbers = [] # положительные
 
+for i in range(numbers):
+    numbers_s = int(input())
+    if numbers_s < 0:
+        negative_results.append(numbers_s)
+    elif numbers_s == 0:
+        zero.append(numbers_s)
+    else:  # number > 0
+        positive_numbers.append(numbers_s)
+
+print(*negative_results, sep='\n')
+print(*zero, sep='\n')
+print(*positive_numbers, sep='\n')
