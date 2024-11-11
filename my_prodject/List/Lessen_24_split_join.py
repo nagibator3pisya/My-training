afeba84d79692cc8cc1547873997fe8e34784a43
Станислав <morevstas02@mail.ru>
2024-11-11 22:46:38 +0300

    Join

---
+++ my_prodject/List/Lessen_24_split_join.py
@@ -0,0 +1,20 @@
+
+
+# languages = "Python,Java,Perl,PHP,Swift"
+# replaces = languages.replace(',',' ') # убираем запятые
+# print(replaces.split())
+
+# преобразование из str в int
+
+# number = input().split()
+# print(number)
+# for i in range(len(number)):
+#     number[i] = int(number[i])
+# print(number)
+
+# необязательный параметр  split(',')
+
+ip = '192,22,55,6'
+
+s = ip.split(',')
+print(s)
