0aa9f1b36e896a9482b52fa60aac4728444c3a48
Станислав <morevstas02@mail.ru>
2024-11-18 22:27:47 +0300

    Всё сразу 2 🌶️ задача

--- my_prodject/List/Lessen_25.py
+++ my_prodject/List/Lessen_25.py
@@ -1,15 +1,38 @@
-# clear
+"""""
+Дополните приведённый ниже код, чтобы он:
 
-# name = ['Timur', 'Gvido', 'Roman', 'Timur', 'Anders', 'Timur']
-# name.clear()
-# print(name)
+Заменил второй (по порядку) элемент списка на 17;
+Добавил числа 4, 5 и 6 в конец списка;
+Удалил первый (по порядку) элемент списка;
+Удвоил список;
+Вставил число 25 по индексу 3;
+Вывел список с помощью функции print()
 
-# copy
+"""""
 
-name = ['Timur', 'Gvido', 'Roman', 'Timur', 'Anders', 'Timur']
-ss = name.copy()
-print(name)
-# print(ss)
+numbers = [8, 9, 10, 11]
+copy_list = numbers.copy()
 
-ss.pop(1)
+
+zamena = copy_list[1] = 17
+print(copy_list, sep='\n')
+
+new_list = [4,5,6]
+insert_list = copy_list.extend(new_list)
+print(copy_list)
+
+del copy_list[0]
+
+print(copy_list)
+
+ss = copy_list * 2
 print(ss)
+
+vctavka = copy_list.insert(3,25)
+
+print(copy_list)
+
+print(copy_list)
+
+
+
