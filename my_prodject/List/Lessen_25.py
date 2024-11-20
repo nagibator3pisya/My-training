a7c9e4ff838712935d10693b1d8c2b7b441088df
Станислав <morevstas02@mail.ru>
2024-11-20 22:49:05 +0300

    Подсчет артиклей

--- my_prodject/List/Lessen_25.py
+++ my_prodject/List/Lessen_25.py
@@ -1,38 +1,14 @@
-"""""
-Дополните приведённый ниже код, чтобы он:
+l = input().split()
 
-Заменил второй (по порядку) элемент списка на 17;
-Добавил числа 4, 5 и 6 в конец списка;
-Удалил первый (по порядку) элемент списка;
-Удвоил список;
-Вставил число 25 по индексу 3;
-Вывел список с помощью функции print()
+for i in range(len(l)):
+    l[i] = int(l[i])
 
-"""""
+min_value = min(l)
+max_value = max(l)
 
-numbers = [8, 9, 10, 11]
-copy_list = numbers.copy()
+min_index = l.index(min_value)
+max_index = l.index(max_value)
 
+l[max_index],l[min_index] = min_value,max_value
 
-zamena = copy_list[1] = 17
-print(copy_list, sep='\n')
-
-new_list = [4,5,6]
-insert_list = copy_list.extend(new_list)
-print(copy_list)
-
-del copy_list[0]
-
-print(copy_list)
-
-ss = copy_list * 2
-print(ss)
-
-vctavka = copy_list.insert(3,25)
-
-print(copy_list)
-
-print(copy_list)
-
-
-
+print(l)
