0baaee6ec4e74663d4fd564a5ab3b4df2ad6d5b2
Станислав <morevstas02@mail.ru>
2024-11-24 21:10:07 +0300

    Генераторы списков

--- my_prodject/List/Lessen_25.py
+++ my_prodject/List/Lessen_25.py
@@ -1,15 +1,36 @@
+# генератор списков
+
+# цикл
+# list = []
+#
+# for i in range(10):
+#     list.append(i)
+#
+# print(list) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# генератор списков
+# list = [i for i in range(10)]
+# print(list)
 
 
-numbers_string = input().split()
+# простой генератор списков с строками
+# str_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+# list = [int(i) for i in str_list]
+#
+# print(list)
 
-for i in range(len(numbers_string)):
-    numbers_string[i] = int(numbers_string[i])
+
+# Создать список, заполненный квадратами целых чисел от 0 до 9 можно так:
+# list = [int(i**2) for i in range(10)]
+# print(list)
 
 
-b = numbers_string.copy()
+# Создать список, заполненный символами строки:
+# list = [i for i in 'привет']
+# print(list)
 
-numbers_string.sort()
-b.sort(reverse=True)
 
-print(*numbers_string)
-print(*b)
+# подсчет строки с помощью len
+
+# list_a = ['aaa','bbb','c']
+# list_b = [len(x) for x in list_a]
+# print(list_b)
