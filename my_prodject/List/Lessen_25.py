75a604d52a0d66a1ee63e6935e3d25ed7e90bf7c
Станислав <morevstas02@mail.ru>
2024-12-03 20:55:16 +0300

    Задача, получить новый список,  содержащий длины строк исходного списка.

--- my_prodject/List/Lessen_25.py
+++ my_prodject/List/Lessen_25.py
@@ -1,16 +1,11 @@
-""""
-Дополните приведенный код, используя списочное выражение так, чтобы 
-получить новый список, содержащий строки исходного списка, 
-где у каждой строки удалён первый символ.
-"""""
+''''
+Дополните приведенный код,
+ используя списочное выражение, так, чтобы получить новый список, 
+ содержащий длины строк исходного списка.
+'''''
 
-keywords = ['False', 'True', 'None', 'and', 'with',
-            'as', 'assert', 'break', 'class', 'continue',
-            'def', 'del', 'elif', 'else', 'except',
-            'finally', 'try', 'for', 'from', 'global', 'if',
-            'import', 'in', 'is', 'lambda', 'nonlocal', 'not',
-            'or', 'pass', 'raise', 'return', 'while', 'yield']
+keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
 
-new_keywords = [x[1::] for x in keywords]
+lengths = [len(lens) for lens in keywords]
 
-print(new_keywords)
+print(lengths)
