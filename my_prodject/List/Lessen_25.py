5415d5794e5416aae240be71022794f6cb495143
Станислав <morevstas02@mail.ru>
2024-12-03 21:30:44 +0300

    Задача, получить новый список,  содержащий длины строк исходного списка.

--- my_prodject/List/Lessen_25.py
+++ my_prodject/List/Lessen_25.py
@@ -1,11 +1,10 @@
 ''''
-Дополните приведенный код,
- используя списочное выражение, так, чтобы получить новый список, 
- содержащий длины строк исходного списка.
+Дополните приведенный код списочным выражением, 
+чтобы получить новый список, содержащий только слова длиной не менее пяти символов (включительно).
 '''''
 
-keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
+keywords = ['ll','False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
 
-lengths = [len(lens) for lens in keywords]
+lengths = [i for i in keywords if len(i) <= 5]
 
 print(lengths)
