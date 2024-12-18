b0faf9c58838522b0c47a8fbff539e10a186a860
Станислав <morevstas02@mail.ru>
2024-12-18 20:39:10 +0300

    Картежи и их методы

--- my_prodject/Tuple/Lessen_27_Tuple.py
+++ my_prodject/Tuple/Lessen_27_Tuple.py
@@ -1,43 +1,187 @@
-
 ''''
-Картежы
+Функция tuple() преобразования
 '''''
-# tuples = ()
-# print(type(tuples))
+
+# list = [1,2,3,4,5]
+# tuple = tuple(list)
+# print(tuple) # (1, 2, 3, 4, 5)
+
 
 '''
-Пример картежев
+Преобразования строки в картеж
+'''
+
+# string = 'привет'
+#
+# tuple_str = tuple(string)
+# print(tuple_str)
+
+# изменения 
+# writer = ('Лев Толстой', 1827)
+# a = list(writer)
+# a[1] = 1985
+# print(tuple(a)) # ('Лев Толстой', 1985)
+
+
+'''
+Особенности кортежей
+
+Кортежи поддерживают те же операции, что и списки, за исключением изменяющих содержимое.
+
+Кортежи поддерживают:
+
+- доступ к элементу по индексу (только для получения значений элементов);
+- методы, в частности index(), count();
+- встроенные функции, в частности len(), sum(), min() и max();
+- срезы;
+- оператор принадлежности in;
+- операторы конкатенации (+) и повторения (*).
+'''
+
+# доступ по индексу
+
+# turtles = ('Mike', 'Leo', 'Don', 'Raph')
+#
+# print(turtles[0])
+# print(turtles[2])
+
+# вложенный картеж и доступ к нему
+
+# input_box = ('firstbox', (15, 150))
+# print(input_box[1][1])
+
+
+'''
+Оператор проверки in
+'''
+# song = ('Roses', 'are', 'Red')
+#
+# print('Привет' in song)
+# print('Roses' in song)
+
+'''
+for цикл в кортежах
+'''
+
+# my_tuple = ('Wise', 'men', 'say', 'only', 'fools', 'rush', 'in')
+# for i in my_tuple:
+#     print(i)
+
+'''
+сортировка
+'''
+
+# sorteds = (100000, 100, 10, 10000, 1, 1000)
+#
+# print(tuple(sorted(sorteds)))
+
+'''
+удаление
+'''
+
+# some_useless_stuff = ('sad', 'bad things', 'trans fats')
+# del some_useless_stuff
+#
+# print(some_useless_stuff)
+
+'''
+Cрезы
+'''
+# float_tuple = (1.1, 0.5, 45.5, 33.33, 9.12, 3.14, 2.73)
+# print(float_tuple[1:3])
+# print(float_tuple[1:])
+# print(float_tuple[:1])
+
+
+'''
+Длинна
+'''
+
+# lens = ('ghb','ggg','gdfdf')
+#
+# print(len(lens))
+
+
+'''
+Конкатенация 
 '''
 #
-# empty_tuple = ()                                     # пустой кортеж
-# point = (1.5, 6.0)                                   # кортеж из двух чисел
-# names = ('Timur', 'Ruslan', 'Roman')                 # кортеж из трех строк
-# info = ('Timur', 'Guev', 28, 170, 60, False)         # кортеж из 6 элементов разных
-# # типов ('Timur', 'Guev', 28, 170, 60, False)
+'''
+сложение
+'''
+# tuple1 = ('ggg',)
+# tuple2 = ('aa',)
+# tuple3 = ('dfsdf',)
 #
-# nested_tuple = (('one', 'two'), ['three', 'four'])   # кортеж из кортежа и списка (('one', 'two'), ['three', 'four'])
+# print(tuple1 + tuple3 + tuple2)
 #
-# print(info)
+'''
+остальные операции
+'''
+# nums = ((1,2,34) + (2,4,5))
+# nums2 = ((1,2)*3)
+# nums3 = ((0,) * 10)
+# print(nums)
+# print(nums2)
+# print(nums3)
+
+'''
+доп операции += *= и тд
+'''
+#
+# a = (1,2)
+# b = (3,4,5)
+#
+# a += b
+# b *= 5
+# print(a)
+# print(b)
+
+'''
+Индекс заданного элемента
+
+Метод index() позволяет получить индекс элемента. 
+Достаточно передать нужное значение элемента, как аргумент метода:
+'''
+# rom = ('I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X')
+# print(rom.index('I'))
 
 
 '''
-Кортеж с одним элементом
+Число вхождений 
+метод count
 '''
-
-# tuple = (1,)
-# print(type(tuple))
+# AT = ('Finn', 'Jake', 'BiMo', 'Marceline', 'Princess Bubblegum', 'BiMo','Jake','Jake')
+# print(AT.count('Jake'))
 
 
 '''
-Изменяем значения в картеже
+Встроенные функции sum(), min(), max()
 '''
+# sum()
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# print(sum(numbers))
+# print(f'минимальное', min(numbers))
+# print(f'максимальное', max(numbers))
+
+'''
+Преобразование в разные типы
+из tuple в str,list,dict
+'''
+# str
+tuples = ('gfgdfg','dfgdss')
+print(','.join(tuples))
+
+# list
+tuples2 = (1,2,3,4,5)
+print(list(tuples2))
+
+# dict
+
+tuples3 = (('Joins',123),('Din',2233))
+
+print(dict((x,y) for x,y in tuples3))
 
 
-my_tuple = (1, 'python', [1, 2, 3])
 
-ss =  my_tuple[2][0] = 100
-sss = my_tuple[2].append(17)
 
-dsds = my_tuple[0] = 2
-
-print(my_tuple)
