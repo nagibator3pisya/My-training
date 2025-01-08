98602727a7fe1fa596c6a23d9a0245241cd94578
Станислав <morevstas02@mail.ru>
2025-01-08 16:54:34 +0300

    Иду дальше по курсу

--- my_prodject/Dict/Lessen_30.py
+++ my_prodject/Dict/Lessen_30.py
@@ -1,74 +1,58 @@
-# создания словаря,словарь
-
-# d = {}
-# dicts = dict()
-#
-# dd = {'key':'value' , 'key1':'value1'}
-
-'''''
-Ключи и значения
-'''''
-
-# ddd = {1:'value',
-#        'key2':2,
-#        (1,2,3):[1,2,3]}
-# print(ddd)
-
-'''
-dict() - создания в месте с ним
-'''
-# keys_and_values = [("Александр", 23), ("Виктория",43),
-#                    ("Евгений", 26), ("Петр", 52), ("Денис", 32)]
-#
-# print(dict(keys_and_values))
-
-# ещё один способ создания
-
-# team_names = ["Александр", "Виктория", "Евгений", "Петр", "Денис"]
-# dict_team_names = dict.fromkeys(team_names,0)
-# print(dict_team_names)
-
-
-
-
-
-'''
-Работа со словарями
-'''
-class_ = {'name': '11a',
-          'students': 20,
-          'teacher': 'Ivan Ivanovich'}
-# # in
-# print('name' in class_)
-# # len
-# print(len(class_))
-
-
-
-# .keys() - получить все ключи словаря
-# .values() - получить все значения словаря
-# .items() - получить все пары ключ-значение словаря
-# перебор словаря
-# for o in class_.keys():
-#     print(o)
-
-# for o in class_.values():
-#     print(o)
-
-# for o in class_.items():
-#     print(o)
-
-# for k,v in class_.items():
-#     print(k,v)
-
-'''
-перевод из словаря в список
-'''
-# print((list(class_.items())))
-# print(type(list(class_.values())))
-# print(type(list(class_.values())))
 
 '''
 Работа с методами
-
 '''
+# clear
+# data = {'a': 1, 'b': 2, 'c': 3}
+# print(data.clear())
+
+# copy
+
+# new_data = data.copy()
+# print(new_data)
+
+
+# fromkeys()
+# froms = ('a','b','c')
+# value = 5
+#
+# new_dict = dict.fromkeys(froms,value)
+# print(new_dict)
+
+# get()
+
+# data = {'a': 5, 'b': 5, 'c': 5}
+# value = data.get('a')
+# print(value)
+
+# items()
+
+# data = {'a': 5, 'b': 5, 'c': 5}
+# value = data.items()
+# print(value)
+
+# pop
+
+# data = {'a': 5, 'b': 5, 'c': 5}
+# pops = data.pop('b')
+# print(pops)
+# print(data)
+
+# popitems
+
+# data = {'a': 5, 'b': 5, 'c': 5}
+# data.popitem()
+# print(data)
+
+
+# setdefault
+# data = {'a': 5, 'b': 5, 'c': 5}
+# value = data.setdefault()
+# print(data)
+# print(value)
+
+# update
+data = {'a': 5, 'b': 5, 'c': 5}
+# data.update({'g': 6})
+# print(data)
+
