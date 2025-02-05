d77a2f7c6358e9a60dda8333450ee1f53aaa7440
Станислав <morevstas02@mail.ru>
2025-02-05 22:03:03 +0300

    Сделал удаление из сета.

--- my_prodject/open_file/City_game/Cities_game.py
+++ my_prodject/open_file/City_game/Cities_game.py
@@ -30,11 +30,13 @@ while bools:
     if user_input == 'Стоп':
         print('Cтоп игра')
         break
-    user_city = set(user_input.split(','))
-    out = user_city.intersection(copy_my_set)
+    user_city = set(user_input.split(',')) # приобразовка в сет
+    out = user_city.intersection(copy_my_set) # сравнение
     if out:
         print(f"Есть такой город: {''.join(out)}")
-        print(f'Удаляю этот город {out.pop()}')
+        dels = out.pop() # удаляет и возвращает его
+        copy_my_set.remove(dels) # окончательно удаляет
+        print(f'Удаляю этот город {dels}')
         print(copy_my_set)
 
     else:
