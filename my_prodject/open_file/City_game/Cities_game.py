cf0fe45edf8e9d5449bbae0a35efb741d99c226f
Станислав <morevstas02@mail.ru>
2025-02-06 22:35:02 +0300

    Сделал ход бота, нужно ещё придумать добавьте правило, что если подходящего города в списке нет, то в силу вступает уже предпоследняя буква, если её нет, то пред-предпоследняя, и так далее.

--- my_prodject/open_file/City_game/Cities_game.py
+++ my_prodject/open_file/City_game/Cities_game.py
@@ -6,11 +6,11 @@ from open_file.City_game.cities import *
 '''
 my_set = {city['name'] for city in cities} # новый вариант
 copy_my_set = my_set.copy()
-count = 0
+copy_list_set = list(copy_my_set)
 
-for i in copy_my_set:
-   count += 1
-print(f'Количество слов: {count}')
+
+bots = copy_my_set.pop() # рандом слово от бота
+print(f'Слова бота: {bots}')
 
 '''
 это то что я придумал старый
@@ -23,22 +23,52 @@ print(f'Количество слов: {count}')
 # new_set = set(list)
 # print(new_set)
 
-bools = True
+bools = False
 
-while bools:
+while not bools:
     user_input = input('Введите название города:').strip()
     if user_input == 'Стоп':
         print('Cтоп игра')
         break
     user_city = set(user_input.split(',')) # приобразовка в сет
     out = user_city.intersection(copy_my_set) # сравнение
-    if out:
-        print(f"Есть такой город: {''.join(out)}")
+
+
+    if not out:
+        print('Такого города нет выиграл бот ')
+        break
+
+    str_out = ''.join(out)
+    if str_out[0].lower() != bots[-1].lower():
+        print(f'Не правильный ответ он должен начинаться на букву {bots[0]}')
+        continue
+    else:
+        print(f'Бот: мне на букву {str_out[-1]}')
         dels = out.pop() # удаляет и возвращает его
         copy_my_set.remove(dels) # окончательно удаляет
+
         print(f'Удаляю этот город {dels}')
-        print(copy_my_set)
+        print(len(copy_my_set))
 
-    else:
-        print('Такого города нет')
+    for i in copy_list_set:
+        if i[0].lower() == str_out[-1]: # Если 1 буква бота равна последней буквы игрока
+            bots = i
+            copy_list_set.remove(i) # Удаляем слово из городов
+            print(f'Бот : {i} , тебе на букву {i[-1]}')
+            break
 
+    # else:
+    #     print(f'Боту на букву {str_out[-1]}: {}')
+
+    # if out:
+    #     print(f"Есть такой город: {str_out}")
+    #     dels = out.pop() # удаляет и возвращает его
+    #     copy_my_set.remove(dels) # окончательно удаляет
+    #     print(f'Удаляю этот город {dels}')
+    #     print(copy_my_set)
+
+    # else:
+    #     print('Такого города нет')
+else:
+    bools = True
+    print('Ты выиграл')
