40c3404f08d435a9ff14e4549d8acb135a2b1fac
Станислав <morevstas02@mail.ru>
2025-03-09 22:40:00 +0300

    Декораторы

--- my_prodject/Def/Всё_наФункции.py
+++ my_prodject/Def/Всё_наФункции.py
@@ -1,81 +1,47 @@
-# def cub(edge_length: float) -> str:
-#     return f'{3 * edge_length ** 2}'
-#
-#
-#
-# print(cub(5.6))
+# замыкание
 
-"""
-Возвращает словарь, в котором ключ - это буква,
-а значение - это количество повторений этой буквы в строке.
-sequence.count(element) - возвращает количество элементов element в последовательности sequence
-"""
-from typing import *
-
-
+# def names():
+#     all_name = []
 #
-# def letters(string: str) -> dict:
-#     """
-#     Функция для подсчета букв в слове
-#     :param string: Строка для подсчета букв
-#     :return: Словарь. Ключ - буква, значение - число
-#     """
-#     res_dict = {}
-#     set_string = set(string)
-#
-#     for letter in set_string:
-#         if letter != ' ':
-#             count = string.count(letter)
-#             res_dict[letter] = count
-#
-#     return res_dict
-#
-# strr = 'Аргентина манит негрА Аргентина манит негрА Аргентина манит негрА Аргентина манит негрА' \
-#          'Аргентина манит негрА Аргентина манит негрААргентина манит негрА '
-# print(letters(strr))
-
-# def get_key_value(dict_:dict,value:Any) -> dict:
-#     """
-#     Функция возвращает ключ и значение, соответствующий переданному значению.
-#     :param dict_: Словарь
-#     :param value: Значение
-#     :return: Словарь
-#     """
-#     for k,v in dict_.items():
-#         if v == value:
-#             return {k: v}
+#     def inner(name:str)->list:
+#         all_name.append(name)
+#         return all_name
+#     return inner
 #
 #
-# # Тестовый датасет
-# full_dict = {
-#     'title': 'Железный человек',
-#     'year': 2008,
-#     'director': 'Джон Фавро',
-#     'screenwriter': 'Марк Фергус и Хоук Остби, Артур Маркам и Мэтт Холлоуэй',
-#     'producer': 'Ави Арад и Кевин Файги',
-#     'stage': 'Первая фаза'
-# }
-#
-# print(get_key_value(full_dict,'Джон Фавро'))
-
-# Глобальная видимость
-# n = 'Глобальная переменная' #Видна везде, и во всех функц
-#
-#
-# def func():
-#     """
-#     переопределение глобальной переменной, тк у локальной больше преимуществ
-#     :return:
-#     """
-#     n = 'переименовали "n" в функции' # облость видимости локальная
-#     print(f'{n}')
-#
-#
-#
-# func()
-# print(f'Это глобальная переменная "{n}"') # глобальная переменная
+# student = names()
+# print(student('Петя'))
+# print(student('Вася'))
+# print(student('Настя'))
 
 
 
+# def counter():
+#     count = 0
+#
+#     def inner(value: int)-> int:
+#         """
+#         nonlocal count: изменяем значение переменной count
+#         :param value:
+#         :return:
+#         """
+#         nonlocal count
+#         count += value
+#         return count
+#     return inner
+#
+# count = counter()
+# print(count(1))
+# print(count(1))
+# print(count(1))
 
 
+def pow_(base):
+    def inner(value):
+        return value ** base
+    return inner
+
+
+if __name__ == '__main__':
+    pw = pow_(2)
+    print(pw(5))
