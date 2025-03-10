445a6fb92176e121d4486591d1a02a5194f5ef64
Станислав <morevstas02@mail.ru>
2025-03-10 22:33:38 +0300

    Декораторы

--- my_prodject/Def/Всё_наФункции.py
+++ my_prodject/Def/Всё_наФункции.py
@@ -1,47 +1,58 @@
-# замыкание
+#декораторы
+# синтаксис
 
-# def names():
-#     all_name = []
+# def my_decorator(func):
+#     def wrapper():
+#         print(f'Дополнительное поведение до вызова {func.__name__}')
+#         func()
+#         print(f'Дополнительное поведение полсе вызова  {func.__name__}')
 #
-#     def inner(name:str)->list:
-#         all_name.append(name)
-#         return all_name
-#     return inner
+#     return wrapper
 #
 #
-# student = names()
-# print(student('Петя'))
-# print(student('Вася'))
-# print(student('Настя'))
+# @my_decorator
+# def hello_func():
+#    print('Helllo')
 
 
-
-# def counter():
-#     count = 0
+# def decorator(func):
+#     def wrapper():
+#         original_result = func()
+#         modific = original_result.upper()
+#         return modific
+#     return wrapper
 #
-#     def inner(value: int)-> int:
-#         """
-#         nonlocal count: изменяем значение переменной count
-#         :param value:
-#         :return:
-#         """
-#         nonlocal count
-#         count += value
-#         return count
-#     return inner
 #
-# count = counter()
-# print(count(1))
-# print(count(1))
-# print(count(1))
+#
+# @decorator
+# def hello_func():
+#    return 'Helllo'
+
+# два декоратора
+# def html_tag(func):
+#     def wrapper():
+#         return f'<strong>{func()}</strong>'
+#     return wrapper
+#
+# def html_tag2(func):
+#     def wrapper():
+#         return f'<b>{func()}</b>'
+#     return wrapper
+#
+# @html_tag
+# @html_tag2
+# def fincion():
+#     return 'Hello'
 
 
-def pow_(base):
-    def inner(value):
-        return value ** base
-    return inner
+
 
 
 if __name__ == '__main__':
-    pw = pow_(2)
-    print(pw(5))
+    # print(hello_func())
+    # альтернатива
+    # hello_funcs = my_decorator(hello_func)
+    # print(hello_funcs())
+    # hello_func()
+    # print(hello_func())
+    # print(fincion())
